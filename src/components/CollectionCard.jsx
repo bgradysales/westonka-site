68e896d44dfa61f8ef130450cb5d29ffea2ef0df
bgradysales <bgradysales@gmail.com>
2026-05-13 08:49:45 -0500

    changes

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -4,11 +4,11 @@ import { Card, CardContent } from "./Card";
 export default function CollectionCard({ item, navigateTo }) {
   return (
     <Card className="overflow-hidden rounded-3xl shadow-md border-stone-200">
-      <img
-        src={item.image}
-        alt={`${item.title} outdoor furniture Minnesota`}
-        className="h-64 w-full object-cover bg-stone-100"
-      />
+  <img
+  src={item.image}
+  alt={item.title}
+  className="h-[420px] w-full object-contain bg-white p-6"
+/>
 
       <CardContent className="p-6">
         <h3 className="text-2xl font-semibold mb-3">{item.title}</h3>
